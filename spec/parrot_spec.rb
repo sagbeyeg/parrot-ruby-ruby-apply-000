d244800999a175a48a09494a5588bdb84cfff12e
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-08-13 21:08:25 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -2,8 +2,8 @@ require_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
 
 def parrot (phrase = "Squawk")
-  puts #{phrase}
-  #{phrase}
+  puts "#{phrase}"
+  "#{phrase}"
   
   
 end
